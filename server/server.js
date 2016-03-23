f166e1d15f1babf99bd786aea7dcc2c36784a7a0
MrJoas97 <j.hoogendijk@hotmail.com>
2016-03-23 13:06:15 +0100

    Er kunnen nu ook andere bestanden op worden gehaald via de server

--- server/server.js
+++ server/server.js
@@ -5,7 +5,9 @@ var fs = require('fs');
 var server = http.createServer(handler);
 
 function handler(request, response) {
-    fs.readFile(__dirname + '/../client/index.html', function(error, data) {
+    console.log(request.url);
+    if(request.url == "/") {
+        fs.readFile(__dirname + '/../client/index.html', function(error, data) {
         if(error) {
             console.log(error);
             response.writeHead(404);
@@ -16,6 +18,20 @@ function handler(request, response) {
         }
         console.log("response sent..");
     });
+    } else {
+        fs.readFile(__dirname + '/../client' + request.url, function(error, data) {
+        if(error) {
+            console.log(error);
+            response.writeHead(404);
+        } else {
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write(data, 'utf-8');
+            response.end();
+        }
+        console.log("response sent..");
+    });
+    }
+
 };
 server.listen(8000);
 console.log("listening on port 8000");
@@ -24,12 +40,15 @@ var ios = io.listen(server);
 
 ios.sockets.on("connection", function(socket) {
   console.log("user connected" + socket.id);
+
   socket.on("message", function (data) {
       console.log(data);
     socket.broadcast.emit("message", data); //to all other connected clients
 //    ios.socket.emit("message", data); //to all connected clients
   });
 });
+
+
 
 var FPS = 1;
 
@@ -40,12 +59,12 @@ var gameLoop = function () {
   var now = Date.now()
   actualTicks++
   if (previousTick + tickLengthMs <= now) {
-
       //Hier komt de code van binnen de loop
-
+      readInput();
       updateClients();
       //Reset loop tijd
     actualTicks = 0
+    previousTick = Date.now();
   }
     //Deze code zorgt ervoor dat de CPU niet constant 100% is :)
   if (Date.now() - previousTick < tickLengthMs - 16) {
@@ -55,10 +74,15 @@ var gameLoop = function () {
   }
 }
 
-//Deze code stuurt info terug naar de clients en zorgt ervoor
-var updateClients() {
+//Read the input of the clients
+var readInput = function() {
 
 }
 
-// begin the loop !
+//Deze code stuurt info terug naar de clients en zorgt ervoor
+var updateClients = function() {
+
+}
+
+// begin the loop!
 gameLoop()
